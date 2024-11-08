25f574829a2c74e3e2b8dde98c20b09efcfbe103
ninanikolaevna <ninanikolaevna.t@gmail.com>
2024-11-09 00:27:38 +0300

    Part2

--- src/NewMain.java
+++ src/NewMain.java
@@ -1,19 +1,9 @@
 public class NewMain {
     public static void main(String[] args) {
-Book book1 =  new Book("Капитанская дочь", 1990, new Author("Александр","Пушкин"));
-        System.out.printf("\"%s\",%s,%s %s",
-                book1.getName(),
-                book1.getYear(),
-                book1.getAuthor().getFirstName(),
-                book1.getAuthor().getLastName()
-        );
-        System.out.println(" ");
-book1.setYear(1999);
-        System.out.printf("\"%s\",%s,%s %s",
-                book1.getName(),
-                book1.getYear(),
-                book1.getAuthor().getFirstName(),
-                book1.getAuthor().getLastName()
-        );
+        Book book1 = new Book("Капитанская дочь", 1999, new Author("Александр", "Пушкин"));
+        System.out.println(book1);
+
+        book1.setYear(1990);
+        System.out.println(book1);
     }
 }
